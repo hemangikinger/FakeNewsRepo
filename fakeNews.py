902f627cc14f9645e9d0478c1c1536435db77514
hemangikinger <hemangikinger@gmail.com>
2020-09-07 19:21:01 +0530

    divided into train and test model

--- fakeNews.py
+++ fakeNews.py
@@ -5,3 +5,15 @@ from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import PassiveAggressiveClassifier
 from sklearn.metrics import accuracy_score, confusion_matrix
+
+#Read the data
+df=pd.read_csv('D:\\DataFlair\\news.csv')
+#Get shape and head
+df.shape
+df.head()
+# Get Labels
+labels=df.label
+labels.head()
+
+#Split the dataset
+x_train,x_test,y_train,y_test=train_test_split(df['text'], labels, test_size=0.2, random_state=7)
